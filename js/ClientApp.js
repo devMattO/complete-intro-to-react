d0ab5afb63651a71938e2491eaa8b87ff6c47e03
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-14 16:47:55 -1000

    initial webpack build set up

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,24 +1,14 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
 var div = React.DOM.div
-var h1 = React.DOM.h1
 
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1(null, this.props.title)
-      )
-    )
-  }
-})
-
-var MyTitleFact = React.creatFactory(MyTitle);
-var ce = React.createElement
+var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
   div(null,
     MyTitleFact({title: 'Props are great!'}),
     React.createElement(MyTitle, {title: 'Use props everywhere!'})
-    ce(MyTitle, {title: 'Props are the best!'})
 
   )
 )
