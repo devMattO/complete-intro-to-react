231b1bb0d90353c11222c84aacae5e57dc0e2e14
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-14 15:56:25 -1000

    made first component with props

---
+++ js/ClientApp.js
@@ -0,0 +1,26 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1(null, this.props.title)
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.creatFactory(MyTitle);
+var ce = React.createElement
+
+var MyFirstComponent = (
+  div(null,
+    MyTitleFact({title: 'Props are great!'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!'})
+    ce(MyTitle, {title: 'Props are the best!'})
+
+  )
+)
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
