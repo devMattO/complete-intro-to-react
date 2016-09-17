6f7ee20fb9ab911d1bb226c8e41ea98561c20d52
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-16 21:55:08 -1000

    using map to create multiple components from a json file

--- js/search.jsx
+++ js/search.jsx
@@ -1,10 +1,20 @@
 const React = require('react')
-const shows = require('../public/data')
+const data = require('../public/data')
 
 const Search = _ => (
-  <pre><code>
-    {JSON.stringify(shows, null, 4)}
-  </code></pre>
+  <div className='container'>
+    <div className='shows'>
+      {data.shows.map(show => (
+        <div className='show-card'>
+          <img src={`public/img/posters/${show.poster}`} className='show-img' />
+          <div classNmae='show-text'>
+            <h3 className="show-title">{show.title}</h3>
+            <h4 className='show-year'>({show.year})</h4>
+            <p className='show-description'>{show.description}</p>
+          </div>
+        </div>))}
+    </div>
+  </div>
 )
 
 module.exports = Search
