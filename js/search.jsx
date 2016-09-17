3bea40df033fea036400d0e831f6ac51b41ed09d
Matt Olsen <olsen_matthew@yahoo.com>
2016-09-17 11:20:14 -1000

    setting up react router with a landing page

--- js/search.jsx
+++ js/search.jsx
@@ -1,18 +1,13 @@
 const React = require('react')
+const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
 const Search = _ => (
   <div className='container'>
     <div className='shows'>
       {data.shows.map(show => (
-        <div className='show-card'>
-          <img src={`public/img/posters/${show.poster}`} className='show-img' />
-          <div classNmae='show-text'>
-            <h3 className="show-title">{show.title}</h3>
-            <h4 className='show-year'>({show.year})</h4>
-            <p className='show-description'>{show.description}</p>
-          </div>
-        </div>))}
+        <ShowCard {...show} key={show.imdbID} />
+      ))}
     </div>
   </div>
 )
